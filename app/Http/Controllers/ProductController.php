c17c89db081b00c742854ed4d6be68358bd3c937
İskender <100355107+iskenderErgul@users.noreply.github.com>
2024-01-17 16:31:59 +0300

    Primevue setup

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Repositories\ProductRepository;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+    protected  $productRepository;
+
+    public function __construct(ProductRepository  $productRepository)
+    {
+        $this->productRepository  = $productRepository;
+    }
+
+    public function getAllProducts(): JsonResponse
+    {
+       $products= $this->productRepository->getAllProducts();
+        return response()->json($products);
+    }
+
+    public function getProduct(Request $request , $id){
+        $product = $this->productRepository->getProduct($id);
+        return response()->json($product);
+    }
+}
