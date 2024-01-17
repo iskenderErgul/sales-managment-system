fc09b9a5cefdf68c8d59533e7a9be5dd08bdd77f
İskender <100355107+iskenderErgul@users.noreply.github.com>
2024-01-17 15:24:06 +0300

    Primevue setup

---
+++ app/Http/Controllers/Auth/LoginController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\LoginRequest;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+
+class LoginController extends Controller
+{
+    public function showLogin() {
+        return view('auth.auth');
+    }
+
+    public function login(LoginRequest $request){
+
+        $user  = User::where('email',$request->email)->first();
+
+        if($user && Hash::check($request->password,$user->password)){
+            Auth::login($user);
+            return redirect('/sys');
+        }else {
+            return  redirect()->route('login')->withErrors([
+                'email' => 'Verdiginiz bilgilerle eşleşen bir kullanıcı bulunamadı'
+            ])
+                ->onlyInput("email");
+        }
+
+    }
+}
